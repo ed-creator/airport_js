4d3ef2a65c94483ab35baed54a7ea93db6ee85f3
kittysquee <kittysquee@gmail.com>
2017-04-11 16:07:53 +0100

    Finishes Plane object

--- js/jasmine/spec/planeSpec.js
+++ js/jasmine/spec/planeSpec.js
@@ -3,6 +3,7 @@ describe('Plane', function(){
   beforeEach(function(){
     plane = new Plane();
     // spyOn(airport, 'airport')
+    airport = jasmine.createSpy('airport')
   });
   describe('flying', function(){
     it('expects flying to be true ', function(){
@@ -13,6 +14,22 @@ describe('Plane', function(){
     });
     it('checks if a plane is landed', function(){
       expect(plane.isLanded()).toBe(false)
+    });
+    it('checks the plane is landed', function(){
+      plane.land(airport)
+      expect(plane.flying).toBe(false)
+      expect(plane.airport).toBe(airport)
+    });
+    it('cannot land a plane, if already landed', function(){
+      plane.flying = false
+      expect(function() { plane.land(airport) ; }).toThrow(('Plane cannot land: plane already landed'))
+    });
+    it('returns the planes airport', function(){
+      plane.land(airport)
+      expect(plane.getAirport()).toEqual(airport)
+    });
+    it('fails to return airport if plane flying', function(){
+      expect(function() { plane.getAirport() ; }).toThrow(('Plane cannot be at an airport: plane already flying'))
     });
   });
 });
--- js/jasmine/src/plane.js
+++ js/jasmine/src/plane.js
@@ -10,3 +10,13 @@ Plane.prototype.takeOff = function() {
 Plane.prototype.isLanded = function() {
   return !(this.flying);
 };
+
+Plane.prototype.land = function(airport) {
+  if (this.isLanded()) {throw ('Plane cannot land: plane already landed')};
+  this.airport = airport
+  this.flying = !this.flying
+};
+Plane.prototype.getAirport = function() {
+  if (this.flying) {throw ('Plane cannot be at an airport: plane already flying')};
+  return this.airport;
+};
